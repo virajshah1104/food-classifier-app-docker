52bb5de41121c7f280152d4ef3cf326f8e060b35
Viraj Shah <virajshah1104@gmail.com>
2022-05-12 13:34:33 -0700

    Changessss

--- app.py
+++ app.py
@@ -62,7 +62,4 @@ def predict():
     return {'success': True, 'category': category}
 
 if __name__ == '__main__':
-    if os.environ.get('APP_LOCATION') == 'heroku':
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    else:
-        app.run(host='localhost', port=8080, debug=True, reloader=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
