d9b804a690af41fc0ebaa07c710df65d7ea8d15f
Viraj Shah <virajshah1104@gmail.com>
2022-05-12 13:00:15 -0700

    message

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM tensorflow/tensorflow
 COPY . /app
 WORKDIR /app
-RUN pip install -r requirements.txt 
+RUN pip install -r requirements.txt
+EXPOSE 3000
+EXPOSE $PORT
 ENTRYPOINT [ "python" ] 
 CMD [ "app.py" ] 
--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def predict():
     return {'success': True, 'category': category}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False) 
+    app.run(host='0.0.0.0', port='3000', debug=False) 
