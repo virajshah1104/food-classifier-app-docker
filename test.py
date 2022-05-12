60693665c2e1ffdaf7d469c53b1a44581dca4bad
Viraj Shah <virajshah1104@gmail.com>
2022-05-12 13:54:53 -0700

    First Complete Version

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import base64
 
 imgb64 = open(sys.argv[1], 'rb').read()
 imgb64 = base64.b64encode(imgb64).decode()
-url = 'http://localhost:8080/predict'
+url = 'https://lit-fjord-92060.herokuapp.com/predict'
 obj = {'image':imgb64}
 x = requests.post(url, json=obj)
 print(x.text)
